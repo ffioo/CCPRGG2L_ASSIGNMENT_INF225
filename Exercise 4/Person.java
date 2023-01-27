0199822d9198b3ad005b4c9187184a1f5271fb94
Ffiona Jealo D. Sayson <120081580+ffioo@users.noreply.github.com>
2023-01-27 10:58:00 +0800

    Create Person.java

---
+++ Exercise 4/Person.java
@@ -0,0 +1,13 @@
+public class Person {
+
+    // properties
+    int age;
+    String name;
+
+    // method
+    Person(String personName, int personAge){
+        this.name = personName;
+        this.age = personAge;
+    }
+
+}
