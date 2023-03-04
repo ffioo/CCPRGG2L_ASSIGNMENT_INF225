7725a825291e1ba783a63eb7dcaa6d03c5045d92
Ffiona Jealo D. Sayson <120081580+ffioo@users.noreply.github.com>
2023-03-04 22:31:49 +0800

    Create GroceryItem.java

---
+++ Exercise 6/GroceryItem.java
@@ -0,0 +1,13 @@
+public class Groceryitem {
+    
+    String name;
+    double price;
+
+    public  void showItemName() {
+            System.out.println(this.name);
+    }
+
+    public  void showPrice(Groceryitem item) {
+        System.out.println(this.price);
+    }
+}
