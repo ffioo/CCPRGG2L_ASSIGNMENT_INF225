06a86f39ef21891ea26cddbe738a91756ed77ee7
Ffiona Jealo D. Sayson <120081580+ffioo@users.noreply.github.com>
2023-03-04 22:32:57 +0800

    Create Toothpaste.java

---
+++ Exercise 6/Toothpaste.java
@@ -0,0 +1,9 @@
+public class Toothpaste extends Groceryitem {
+
+    public  void showItemName() {
+        System.out.println(this.name + " is a Toothpaste");
+    }
+    public  void showPrice(Groceryitem item) {
+        System.out.println("Price " + this.price);
+    }
+}
